238eb45664b4fb34bb38b3e5e3cc1b6a5434fe03
Elena Galevskaja <83348388+PalarLine@users.noreply.github.com>
2023-10-24 23:00:15 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+
+function openTasks1() {
+    let elem = document.getElementById('hidden1')
+    elem.style.display = (elem.style.display == "block") ? "none" : "block"
+    
+
+}
+function openTasks2() {
+    let elem = document.getElementById('hidden2')
+    elem.style.display = (elem.style.display == "block") ? "none" : "block"
+}
