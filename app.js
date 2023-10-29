b66cc3f1999e20855be2494dd494a71ddd74f526
palarline <kennelpalarline@gmail.com>
2023-10-29 18:33:46 +0200

    29.10.2023_1

---
+++ app.js
@@ -0,0 +1,7 @@
+
+function openTasks(index) {
+        let elem = document.getElementById('tasks' + index)
+        let display = elem.style.display
+        elem.style.display = (display === "block") ? "none" : "block"
+}
+                
